60dde9befed5eb690384f6a2267b0c068a603316
kulius <kullius@msn.com>
2025-12-03 22:07:45 +0800

    3.16 更新

---
+++ book_addons/3.16/estate/models/estate_property_tag.py
@@ -0,0 +1,15 @@
+# in estate/models/estate_property_tag.py
+from odoo import fields, models
+
+class EstatePropertyTag(models.Model):
+    _name = "estate.property.tag"
+    _description = "Real Estate Property Tag"
+    _order = "name" # 新增這一行
+    
+    _unique_name = models.Constraint(
+    'UNIQUE(name)',
+    'A property tag name must be unique.',)
+
+    name = fields.Char(required=True)
+    
+    
